7fd921ef048e7b72b7a82b678a4d9527328929a7
770menahem <mnjol770@gmail.com>
2021-07-28 15:45:05 +0300

    delete schedule from aka

--- aka/src/DataAccess/initializeSchedule.ts
+++
@@ -1,8 +0,0 @@
-import repoSf from '../mongo/repo.aka';
-import { DbHandler } from '../../../utils/scheduleGetData';
-import getAkaData from './getAkaData';
-
-export default async () => {
-  const schedule = new DbHandler(repoSf.update, getAkaData);
-  schedule.start();
-};
